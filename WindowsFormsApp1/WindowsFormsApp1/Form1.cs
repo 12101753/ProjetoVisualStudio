1cb2241fa8250725c402b25557e575d99a6a9457
12101753 <105506847+12101753@users.noreply.github.com>
2022-05-13 12:20:29 -0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmDados : Form
+    {
+        public FrmDados()
+        {
+            InitializeComponent();
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicou no txt");
+        }
+
+        private void txtBoleano_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicou no txt");
+        }
+
+        private void txtTexto_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicou no txt");
+        }
+
+        private void lblDecimal_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicou no Lbl");
+        }
+    }
+}
